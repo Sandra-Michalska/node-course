d0965d88f5431ad0c0d1064844310ac61411ce9d
Sandra Michalska <sandra.kinga.michalska@gmail.com>
2024-05-13 11:35:20 +0200

    Add Express Router

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const express = require('express');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const bodyParser = require('body-parser');
+
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log('##req body', req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 
 app.listen(3000);
---
+++ routes/shop.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+router.get('/', (req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
+
+module.exports = router;
